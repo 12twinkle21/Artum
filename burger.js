140cd1a8e4bcd9e1ae7a77cfe67f253487f9704c
12twinkle21 <arluncov@yandex.ru>
2021-10-22 20:42:10 +0300

    add new files

---
+++ burger.js
@@ -0,0 +1,32 @@
+let burgerBtn = document.querySelector('.nav-burger');
+let burgerMenu = document.querySelector('.burger-menu');
+
+
+
+burgerBtn.addEventListener('click', function (evt) {
+   evt.preventDefault();
+   evt.stopPropagation();
+   if (burgerMenu.classList.contains('no-active-menu')) {
+      burgerMenu.classList.add('active-menu');
+      burgerMenu.classList.remove('no-active-menu');
+   }
+   else {
+      burgerMenu.classList.add('no-active-menu');
+      burgerMenu.classList.remove('active-menu');
+   }
+})
+
+window.addEventListener('click', function (evt) {
+   if (burgerMenu.classList.contains('active-menu')) {
+      burgerMenu.classList.remove('active-menu');
+      burgerMenu.classList.add('no-active-menu')
+   }
+})
+
+
+
+  
+
+
+
+
