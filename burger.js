d6418dc79c4b75ee9716575789ba418845a330f7
12twinkle21 <arluncov@yandex.ru>
2021-11-10 21:01:52 +0300

    add alert

--- burger.js
+++ burger.js
@@ -8,22 +8,22 @@ burgerBtn.addEventListener('click', function (evt) {
    if (burgerMenu.classList.contains('no-active-menu')) {
       burgerMenu.classList.add('active-menu');
       burgerMenu.classList.remove('no-active-menu');
+      
    }
    else {
       burgerMenu.classList.add('no-active-menu');
       burgerMenu.classList.remove('active-menu');
-
-      window.addEventListener('click', function () {
-         if (burgerMenu.classList.contains('active-menu')) {
-            burgerMenu.classList.remove('active-menu');
-            burgerMenu.classList.add('no-active-menu')
-         }
-      })
+      
    }
 })
 
-
-
+window.addEventListener('click', function () {
+   if (burgerMenu.classList.contains('active-menu')) {
+      burgerMenu.classList.remove('active-menu');
+      burgerMenu.classList.add('no-active-menu')
+      alert('кек');
+   }
+})
 
 
   
